0adb57252f2e10285373e5bd6ef395b1d82bc7bf
gsistelos <gabrielsistelos@gmail.com>
2023-11-16 14:58:45 -0300

    feat: simple restful API

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Hello from API!'
+    });
+});
+ 
+app.get('/user', (req, res) => {
+    const { name } = req.query;
+
+    if (!name) {
+        return res.status(400).send({
+            error: `No name sent!`
+        });
+    }
+
+    res.status(200).send({
+        message: `Hello ${name}!`
+    });
+});
+
+app.post('/secret', (req, res) => {
+    const { name } = req.body;
+    const { secret } = req.body;
+
+    if (!name) {
+        return res.status(400).send({
+            error: `No name sent!`
+        });
+    }
+
+    if (!secret) {
+        return res.status(400).send({
+            error: `No secret sent!`
+        });
+    }
+
+    res.status(200).send({
+        message: `${name}, your secret "${secret}" was sent to the server!`
+    });
+});
+
+app.listen(
+    PORT,
+    () => console.log(`Running on http://localhost:${PORT}`)
+);
