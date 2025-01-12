5bfad5d9b7b87d6783c0a71c40a0dc346f185508
gsistelos <gsistelos@proton.me>
2025-01-11 22:37:02 -0300

    refactor into files

--- src/index.js
+++ src/index.js
@@ -1,120 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const app = express();
-
 const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const User = require("./User");
+const app = express();
 
+const usersRouter = require("./routers/usersRouter");
+
+// Middlewares
 app.use(express.json());
 
-app.get("/api/users", async (req, res) => {
-	try {
-		const users = req.query ? await User.find(req.query) : await User.find();
-
-		if (users.length === 0) {
-			return res.status(404).send({
-				message: "No users found",
-			});
-		}
-
-		res.status(200).send(users);
-	} catch (err) {
-		res.status(500).send({
-			message: err.message,
-		});
-	}
-});
-
-app.get("/api/users/:id", async (req, res) => {
-	try {
-		const user = await User.findOne({ _id: req.params.id });
-
-		if (!user) {
-			return res.status(404).send({
-				message: "User not found",
-			});
-		}
-
-		res.status(200).send(user);
-	} catch (err) {
-		res.status(500).send({
-			message: err.message,
-		});
-	}
-});
-
-app.post("/api/users", async (req, res) => {
-	try {
-		const user = new User(req.body);
-
-		const err = user.validateSync();
-
-		if (err) {
-			return res.status(400).send({
-				message: err.message,
-			});
-		}
-
-		await user.save();
-
-		res.status(201).send({
-			message: "User created",
-		});
-	} catch (err) {
-		res.status(500).send({
-			message: err.message,
-		});
-	}
-});
-
-app.patch("/api/users/:id", async (req, res) => {
-	try {
-		const info = await User.updateOne(
-			{ _id: req.params.id },
-			{ $set: req.body }
-		);
-
-		if (info.modifiedCount === 0) {
-			return res.status(304).end();
-		}
-
-		await User.updateOne(
-			{ _id: req.params.id },
-			{ $currentDate: { updatedAt: true } }
-		);
-
-		res.status(200).send({
-			message: "User updated",
-		});
-	} catch (err) {
-		res.status(500).send({
-			message: err.message,
-		});
-	}
-});
-
-app.delete("/api/users/:id", async (req, res) => {
-	try {
-		const info = await User.deleteOne({ _id: req.params.id });
-
-		if (info.deletedCount === 0) {
-			return res.status(404).send({
-				message: "User not found",
-			});
-		}
-
-		res.status(200).send({
-			message: "User deleted",
-		});
-	} catch (err) {
-		res.status(500).send({
-			message: err.message,
-		});
-	}
-});
+// Routes
+app.use("/api/users", usersRouter);
 
 mongoose
 	.connect(MONGODB_URI)
---
+++ src/routers/usersRouter.js
@@ -0,0 +1,113 @@
+const { Router } = require("express");
+
+const User = require("../models/User");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+	try {
+		const users = req.query ? await User.find(req.query) : await User.find();
+
+		if (users.length === 0) {
+			return res.status(404).send({
+				message: "No users found",
+			});
+		}
+
+		res.status(200).send(users);
+	} catch (err) {
+		return res.status(500).send({
+			message: err.message,
+		});
+	}
+});
+
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id });
+
+		if (!user) {
+			return res.status(404).send({
+				message: "User not found",
+			});
+		}
+
+		res.status(200).send(user);
+	} catch (err) {
+		return res.status(500).send({
+			message: err.message,
+		});
+	}
+});
+
+router.post("/", async (req, res) => {
+	try {
+		const user = new User(req.body);
+
+		const err = user.validateSync();
+
+		if (err) {
+			return res.status(400).send({
+				message: err.message,
+			});
+		}
+
+		await user.save();
+
+		res.status(201).send({
+			message: "User created",
+		});
+	} catch (err) {
+		return res.status(500).send({
+			message: err.message,
+		});
+	}
+});
+
+router.patch("/:id", async (req, res) => {
+	try {
+		const info = await User.updateOne(
+			{ _id: req.params.id },
+			{ $set: req.body }
+		);
+
+		if (info.modifiedCount === 0) {
+			return res.status(304).end();
+		}
+
+		await User.updateOne(
+			{ _id: req.params.id },
+			{ $currentDate: { updatedAt: true } }
+		);
+
+		res.status(200).send({
+			message: "User updated",
+		});
+	} catch (err) {
+		return res.status(500).send({
+			message: err.message,
+		});
+	}
+});
+
+router.delete("/:id", async (req, res) => {
+	try {
+		const info = await User.deleteOne({ _id: req.params.id });
+
+		if (info.deletedCount === 0) {
+			return res.status(404).send({
+				message: "User not found",
+			});
+		}
+
+		res.status(200).send({
+			message: "User deleted",
+		});
+	} catch (err) {
+		return res.status(500).send({
+			message: err.message,
+		});
+	}
+});
+
+module.exports = router;
