b2e0e54f2cb3fcafd1f42152cec84016bd59f868
gsistelos <gsistelos@proton.me>
2025-01-18 00:22:59 +0000

    refactor: change findOne to findById

--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -24,7 +24,7 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findOne({ _id: req.params.id });
+    const user = await User.findById(req.params.id);
 
     if (!user) {
       return res.status(404).send({
